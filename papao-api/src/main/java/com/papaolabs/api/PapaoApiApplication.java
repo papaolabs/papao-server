b9aeafa76342038742a60b22a15c1559d1f332b9
bample <inhwan.lee@outlook.com>
2017-09-02 22:28:38 +0900

    #1 프로젝트 세팅
    - springboot initializer 사용
    - .gitignore
    - 체크스타일 관련 파일 추가

---
+++ papao-api/src/main/java/com/papaolabs/api/PapaoApiApplication.java
@@ -0,0 +1,12 @@
+package com.papaolabs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapaoApiApplication.class, args);
+	}
+}
