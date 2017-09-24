ca93e31fbcb114ed31c4b7efb41b3ce0ef89e734
bample <inhwan.lee@outlook.com>
2017-09-24 18:28:22 +0900

    entity 별 생성/수정일 등록

--- papao-api/src/main/java/com/papaolabs/api/domain/model/Kind.java
+++ papao-api/src/main/java/com/papaolabs/api/domain/model/Kind.java
@@ -1,14 +1,16 @@
 package com.papaolabs.api.domain.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
+@EntityListeners(value = { AuditingEntityListener.class })
 @Table(name = "KIND_INFO_TB")
 public class Kind {
     @Id
@@ -17,4 +19,10 @@ public class Kind {
     private Long upKindCode;
     private Long kindCode;
     private String kindName;
+    @Column(name = "CREATED_DATE")
+    @CreatedDate
+    private Date createdDate;
+    @Column(name = "UPDATED_DATE")
+    @LastModifiedDate
+    private Date lastModifiedDate;
 }
--- papao-api/src/main/java/com/papaolabs/api/domain/model/Shelter.java
+++ papao-api/src/main/java/com/papaolabs/api/domain/model/Shelter.java
@@ -1,14 +1,16 @@
 package com.papaolabs.api.domain.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
+@EntityListeners(value = { AuditingEntityListener.class })
 @Table(name = "SHELTER_INFO_TB")
 public class Shelter {
 
@@ -21,4 +23,10 @@ public class Shelter {
     private String townName;
     private Long shelterCode;
     private String shelterName;
+    @Column(name = "CREATED_DATE")
+    @CreatedDate
+    private Date createdDate;
+    @Column(name = "UPDATED_DATE")
+    @LastModifiedDate
+    private Date updatedDate;
 }
