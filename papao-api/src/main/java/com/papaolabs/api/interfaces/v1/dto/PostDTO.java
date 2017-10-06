a99a6092a9a7d9fddb8418b54eabc27d20f1bb35
bample <inhwan.lee@outlook.com>
2017-10-07 01:13:56 +0900

    #26 PostDTO NON_NULL 인 경우만 해당 필드 리턴

--- papao-api/src/main/java/com/papaolabs/api/interfaces/v1/dto/PostDTO.java
+++ papao-api/src/main/java/com/papaolabs/api/interfaces/v1/dto/PostDTO.java
@@ -1,10 +1,14 @@
 package com.papaolabs.api.interfaces.v1.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Value;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Builder
 @Value
+@JsonInclude(NON_NULL)
 public class PostDTO {
     private Long id;
     private String type;
