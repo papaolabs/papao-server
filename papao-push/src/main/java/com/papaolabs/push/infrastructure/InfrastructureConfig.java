e55b9ca72e15fc1e62ab5ae8ea8375c388d7c5f3
bample <inhwan.lee@outlook.com>
2017-11-13 01:01:51 +0900

    #126 push log create,update date 설정

--- papao-push/src/main/java/com/papaolabs/push/infrastructure/InfrastructureConfig.java
+++ papao-push/src/main/java/com/papaolabs/push/infrastructure/InfrastructureConfig.java
@@ -1,11 +1,14 @@
 package com.papaolabs.push.infrastructure;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.papaolabs.push.infrastructure.persistence.jpa.JpaConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@Import({
+            JpaConfiguration.class
+        })
 public class InfrastructureConfig {
 }
