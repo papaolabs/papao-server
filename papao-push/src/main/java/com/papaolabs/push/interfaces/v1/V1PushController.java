bda6d625b626a4a4133cc776d7813f3d5760ae8d
bample <inhwan.lee@outlook.com>
2017-11-12 11:37:44 +0900

    #126 push controller 작성

--- papao-push/src/main/java/com/papaolabs/push/interfaces/v1/V1PushController.java
+++ papao-push/src/main/java/com/papaolabs/push/interfaces/v1/V1PushController.java
@@ -1,4 +1,48 @@
 package com.papaolabs.push.interfaces.v1;
 
+import com.papaolabs.push.domain.model.PushRequest;
+import com.papaolabs.push.domain.service.PushService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/push")
 public class V1PushController {
+    @NotNull
+    private final PushService pushService;
+
+    public V1PushController(PushService pushService) {
+        this.pushService = pushService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity sendPush(@RequestParam PushRequest pushRequest) {
+        this.pushService.sendPush(pushRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/send/list")
+    public ResponseEntity sendPush(@RequestParam List<PushRequest> pushRequests) {
+        this.pushService.sendPush(pushRequests);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity ownPushList(@RequestParam String userId) {
+        return new ResponseEntity(this.pushService.getOwnPushLogs(userId), HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity deletePush(@RequestParam String pushId) {
+        this.pushService.deletePushLog(pushId);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
